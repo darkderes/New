5ad76b53bae080a839284aa424378a7624f57487
Jorge Darderes <jdarderes@gmail.com>
2021-07-30 20:10:10 -0400

    Proyecto terminado.. Aunque icon por archivos Font no es visto en los equipos Sansung Galaxy

--- New/New.Android/MainActivity.cs
+++ New/New.Android/MainActivity.cs
@@ -9,11 +9,13 @@ using Android.OS;
 
 namespace New.Droid
 {
-    [Activity(Label = "New", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "New", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
